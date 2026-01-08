cb06db27120ff29200d589633fc2a35887a1feb0
Stanislav Levin <stanislav2708@outlook.de>
2026-01-08 19:34:17 +0100

    create footer for legal notice links

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent {
+  currentRoute = '';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.currentRoute = this.router.url;
+    });
+  }
+
+  get footerClass(): string {
+    if (this.currentRoute.includes('/home')) return 'footer-home';
+    if (this.currentRoute.includes('/step1')) return 'footer-step1';
+    if (this.currentRoute.includes('/step2')) return 'footer-step2';
+    if (this.currentRoute.includes('/results')) return 'footer-results';
+    if (this.currentRoute.includes('/cookbook')) return 'footer-cookbook';
+    if (this.currentRoute.includes('/recipe')) return 'footer-recipe';
+    return 'footer-default';
+  }
+}
